d63a1b239cae25ba6e8f5637cf9f0eead5bdf7ad
Nicky Flello <nicky_flello@hotmail.com>
2020-03-02 19:16:36 -0800

    Revert "added green masks to display bad cut side"
    This reverts commit 95a1d0b45a816c9028c91a81c497b15b3fc70719.

--- NoteSliceVisualizer/SliceController.cs
+++ NoteSliceVisualizer/SliceController.cs
@@ -8,14 +8,12 @@ namespace NoteSliceVisualizer
 	public class SliceController
 	{
 		private readonly float _uiScale;
-		private readonly RectTransform _blockTransform;
-		private readonly RectTransform _maskTransform;
-		private readonly RectTransform _sliceTransform;
-		private readonly CanvasGroup _canvasGroup;
-		private readonly RawImage _backgroundImage;
-		private readonly RawImage _noteSliceImage;
-		private readonly RawImage _maskLeftImage;
-		private readonly RawImage _maskRightImage;
+		private RectTransform _blockTransform;
+		private RectTransform _maskTransform;
+		private RectTransform _sliceTransform;
+		private CanvasGroup _canvasGroup;
+		private RawImage _backgroundImage;
+		private RawImage _noteSliceImage;
 		private Color _backgroundColor;
 		private float _timeSinceSliced;
 
@@ -23,7 +21,6 @@ namespace NoteSliceVisualizer
 		private readonly Color _cutLineColor = ConfigHelper.Config.CutLineColor;
 		private readonly float _cutLineWidth = ConfigHelper.Config.CutLineWidth;
 		private readonly float _cutLineLengthScale = ConfigHelper.Config.CutLineLengthScale;
-
 
 		private readonly bool _shouldUpdateColor = !ConfigHelper.Config.TwoNoteMode;
 		private readonly float _maxAlpha = ConfigHelper.Config.Alpha;
@@ -34,15 +31,13 @@ namespace NoteSliceVisualizer
 		public SliceController(GameObject canvas)
 		{
 			_blockTransform = canvas.GetComponentsInChildren<RectTransform>().First(o => o.name == "Block");
-			_maskTransform = canvas.GetComponentsInChildren<RectTransform>().First(o => o.name == "NoteMask");
+			_maskTransform = canvas.GetComponentsInChildren<RectTransform>().First(o => o.name == "Mask");
 			_sliceTransform = canvas.GetComponentsInChildren<RectTransform>().First(o => o.name == "NoteSlice");
-			_noteSliceImage = canvas.GetComponentsInChildren<RectTransform>().First(o => o.name == "NoteSliceImage").GetComponent<RawImage>();
-			_maskLeftImage = canvas.GetComponentsInChildren<RectTransform>().First(o => o.name == "MaskLeft").GetComponent<RawImage>();
-			_maskRightImage = canvas.GetComponentsInChildren<RectTransform>().First(o => o.name == "MaskRight").GetComponent<RawImage>();
 			_canvasGroup = _blockTransform.GetComponent<CanvasGroup>();
-			_uiScale = _blockTransform.rect.width * 1.1f;
+			_uiScale = _blockTransform.rect.width;
 
 			_backgroundImage = _blockTransform.GetComponent<RawImage>();
+			_noteSliceImage = _sliceTransform.GetComponent<RawImage>();
 			_noteSliceImage.texture = _cutLineTexture;
 			_noteSliceImage.color = _cutLineColor;
 
@@ -60,36 +55,18 @@ namespace NoteSliceVisualizer
 		{
 			_backgroundColor = color;
 			_backgroundImage.color = color;
-			//_backgroundColor = Color.white;
-			//_backgroundImage.color = Color.white;
 		}
 
-		public void UpdateSlice(Vector3 cutPoint, Vector3 cutPlaneNormal)
+		public void UpdateSlice(Vector3 cutPoint, Vector3 normal)
 		{
 			cutPoint = new Vector3(cutPoint.x, cutPoint.y);
-			Vector3 uiCutPoint = cutPoint * _uiScale;
+			normal = new Vector3(-normal.y, normal.x).normalized;
 
-			Vector3 cutDirection = new Vector3(-cutPlaneNormal.y, cutPlaneNormal.x).normalized;
-			float uiRotation = Mathf.Atan2(cutDirection.y, cutDirection.x) * Mathf.Rad2Deg;
+			Vector3 start = cutPoint * _uiScale * 1.1f;
+			float rot = Mathf.Atan2(normal.y, normal.x) * Mathf.Rad2Deg;
 
-			_sliceTransform.localPosition = uiCutPoint;
-			_sliceTransform.localRotation = Quaternion.Euler(0f, 0f, uiRotation);
-
-			Vector3 lineNormal = new Vector3(cutPlaneNormal.x, cutPlaneNormal.y);
-			float d = Vector3.Dot(lineNormal, -cutPoint);
-
-			float alpha = Mathf.Clamp01(cutPoint.magnitude / 0.3f);
-
-			//if (d > 0f)
-			//{
-			//	_maskLeftImage.color = new Color(_maskLeftImage.color.r, _maskLeftImage.color.g, _maskLeftImage.color.b, 0f);
-			//	_maskRightImage.color = new Color(_maskRightImage.color.r, _maskRightImage.color.g, _maskRightImage.color.b, alpha);
-			//}
-			//else
-			//{
-			//	_maskLeftImage.color = new Color(_maskLeftImage.color.r, _maskLeftImage.color.g, _maskLeftImage.color.b, alpha);
-			//	_maskRightImage.color = new Color(_maskRightImage.color.r, _maskRightImage.color.g, _maskRightImage.color.b, 0f);
-			//}
+			_sliceTransform.localPosition = start;
+			_sliceTransform.localRotation = Quaternion.Euler(0f, 0f, rot);
 
 			_timeSinceSliced = 0f;
 		}
